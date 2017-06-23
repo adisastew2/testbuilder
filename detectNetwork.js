60a3b7ad162a20566c201de7901800da13ce2172
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-06-23 15:11:41 -0700

    solve Diner's Club & American Express

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,6 +11,13 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  //Visa always has a prefix of 4 and a length of 13, 16, or 19.
+
+  if(cardNumber.length === 14 && (cardNumber.substring(0,2) === '38' || cardNumber.substring(0,2) === '39')){
+    return "Diner's Club";
+  } else if (cardNumber.length === 15 && (cardNumber.substring(0,2) === '34' || cardNumber.substring(0,2) === '37')){
+    return 'American Express';
+  }
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
