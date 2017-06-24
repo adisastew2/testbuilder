a199f6c0a4d97431fedd4645f76c43c47d758c34
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-06-23 20:33:06 -0700

    allow detectNetwork to check for discover

--- detectNetwork.js
+++ detectNetwork.js
@@ -9,17 +9,36 @@
 
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
+
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
+  var isDC = cardNumber.length === 14 && (cardNumber.substring(0,2) === '38' || cardNumber.substring(0,2) === '39');
+
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  var isAE = cardNumber.length === 15 && (cardNumber.substring(0,2) === '34' || cardNumber.substring(0,2) === '37');
+
   //Visa always has a prefix of 4 and a length of 13, 16, or 19.
+  var isV = (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19 ) && (cardNumber.substring(0,1) === '4');
 
-  if(cardNumber.length === 14 && (cardNumber.substring(0,2) === '38' || cardNumber.substring(0,2) === '39')){
+  //MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+  var isMC = cardNumber.length === 16 && (cardNumber.substring(0,2) === '51' || cardNumber.substring(0,2) === '52' || cardNumber.substring(0,2) === '53' || cardNumber.substring(0,2) === '54' || cardNumber.substring(0,2) === '55');
+
+  //Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
+  var DPrefix = (cardNumber.substring(0,4) === '6011' || (Number(cardNumber.substring(0,3)) > 643 && Number(cardNumber.substring(0,3)) < 650) || cardNumber.substring(0,2) === '65');
+  var isD = DPrefix && cardNumber.length === 16 || cardNumber.length === 19;
+  //Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19
+
+  if(isDC){
     return "Diner's Club";
-  } else if (cardNumber.length === 15 && (cardNumber.substring(0,2) === '34' || cardNumber.substring(0,2) === '37')){
+  } else if (isAE){
     return 'American Express';
-  }
+  } else if(isV){
+    return 'Visa';
+  } else if(isMC){
+    return 'MasterCard';
+  } else if(isD){
+    return 'Discover';
+  } 
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
 
 
